d2d85301ea509115f4da17e229917a3da47e68a4
juan <juanjesusperezcastillo@gmail.com>
2023-08-08 18:31:06 -0300

    Tercer Avance 1.2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 let pagina = 1;
 let peliculas = "";
 let ultimaPelicula;
-const Carrito = [];
+let Carrito = [];
 
 // Elementos/etiquetas del dom a utilizar en el proyecto
 const carritoTemplate = document.querySelector("#template");
@@ -35,48 +35,39 @@ const cargarPeliculas = async () => {
       `https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`
     );
     // Si la respuesta es correcta
-    if (respuesta.status === 200) {
-      const datos = await respuesta.json();
-      datos.results.forEach((item) => {
-        const max = 1000;
-        let precio = Math.floor(Math.random() * max);
-        const clone = cardsTemplate.content.cloneNode(true);
-        clone.querySelector(
-          ".poster"
-        ).src = `https://image.tmdb.org/t/p/w500/${item.poster_path}`;
-        clone.querySelector(".titulo").textContent = item.title;
-        clone.querySelector(".p").textContent = `Precio: $${precio}`;
+    const datos = await respuesta.json();
+    datos.results.forEach((item) => {
+      const max = 1000;
+      let precio = Math.floor(Math.random() * max);
+      const clone = cardsTemplate.content.cloneNode(true);
+      clone.querySelector(
+        ".poster"
+      ).src = `https://image.tmdb.org/t/p/w500/${item.poster_path}`;
+      clone.querySelector(".titulo").textContent = item.title;
+      clone.querySelector(".p").textContent = `Precio: $${precio}`;
 
-        clone.querySelector(".btn-primary").dataset.precio = precio;
-        clone.querySelector(".btn-primary").dataset.name = item.title;
-        clone.querySelector(".btn-primary").dataset.id = item.id;
-        fragment.appendChild(clone);
-      });
-      contenedorCards.appendChild(fragment);
-      // Limite de paginas que quiero mostrar
-      if (pagina < 1) {
-        if (ultimaPelicula) {
-          observador.unobserve(ultimaPelicula);
-        }
-        const peliculasEnPantalla = document.querySelectorAll(
-          ".contenedor .pelicula"
-        );
-        ultimaPelicula = peliculasEnPantalla[peliculasEnPantalla.length - 1];
-        observador.observe(ultimaPelicula);
+      clone.querySelector(".btn-primary").dataset.precio = precio;
+      clone.querySelector(".btn-primary").dataset.name = item.title;
+      clone.querySelector(".btn-primary").dataset.id = item.id;
+      fragment.appendChild(clone);
+    });
+    contenedorCards.appendChild(fragment);
+    // Limite de paginas que quiero mostrar
+    if (pagina < 1) {
+      if (ultimaPelicula) {
+        observador.unobserve(ultimaPelicula);
       }
+      const peliculasEnPantalla = document.querySelectorAll(
+        ".contenedor .pelicula"
+      );
+      ultimaPelicula = peliculasEnPantalla[peliculasEnPantalla.length - 1];
+      observador.observe(ultimaPelicula);
     }
   } catch (error) {
     console.log(error);
   }
 };
 cargarPeliculas();
-
-// PAGINA DE CARRITO
-document.addEventListener("click", (e) => {
-  if (e.target.matches(".btn-primary")) {
-    agregarPeliculaCarrito(e);
-  }
-});
 
 const agregarPeliculaCarrito =  (e) => {
   const pelicula = {
@@ -97,11 +88,47 @@ const mostrarCarrito = () => {
     const clone = carritoTemplate.content.cloneNode(true);
     clone.querySelector(".list-group-item .badge").textContent = item.cantidad;
     clone.querySelector(".lead").textContent = item.name;
-    clone.querySelector(".lead span").textContent = item.precio;
+    clone.querySelector(".lead span").textContent = item.precio * item.cantidad;
 
+    clone.querySelector(".btn-success").dataset.id = item.id
+    clone.querySelector(".btn-danger").dataset.id = item.id
     fragment.appendChild(clone);
   });
   carritoContenedor.appendChild(fragment);
 };
 
+const quitarBtn = (e) =>{
+  Carrito = Carrito.filter((item)=>{
+    if(e.target.dataset.id === item.id){
+        if(item.cantidad > 0){
+            item.cantidad--
+        }if(item.cantidad === 0)return 
+        return item
+    }else{
+        return item
+    }
+})
+mostrarCarrito()
+} 
 
+const agregarBtn = (e) =>{
+  Carrito.map((item)=>{
+    e.target.dataset.id === item.id ? item.cantidad++ : item
+  })
+
+  mostrarCarrito()
+}
+//Delegacion de Eventos
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".btn-primary")) {
+    agregarPeliculaCarrito(e);
+  }
+
+  if(e.target.matches(".btn-danger")){
+    quitarBtn(e)
+  }
+
+  if(e.target.matches(".btn-success")){
+    agregarBtn(e)
+  }
+});
