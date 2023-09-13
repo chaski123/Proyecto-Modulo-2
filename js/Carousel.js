eeca37a7408d06190c2b2aef43c90562118bc1ce
jackshaw <scorpion0846@gmail.com>
2023-09-13 02:04:46 -0300

    cambios en el codigo

--- js/Carousel.js
+++ js/Carousel.js
@@ -1,47 +1,44 @@
 // Carousel
-const btnLeft = document.querySelector(".btn-left"),
-      btnRight = document.querySelector(".btn-right"),
-      slider = document.querySelector("#slider"),
-      sliderSection = document.querySelectorAll(".slider-section");
-
-
-btnLeft.addEventListener("click", e => moveToLeft())
-btnRight.addEventListener("click", e => moveToRight())
-
-setInterval(() => {
-    moveToRight()
-}, 3200);
-
-let operacion = 0,
-    counter = 0,
-    widthImg = 100 / sliderSection.length;
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const slider = document.querySelector("#slider");
+const sliderSection = document.querySelectorAll(".slider-section");
 
 const moveToRight = () => {
-    if (counter >= sliderSection.length-1) {
+    if (counter >= sliderSection.length - 1) {
         counter = 0;
         operacion = 0;
         slider.style.transform = `translate(-${operacion}%)`;
         slider.style.transition = "none";
         return;
-    } 
+    }
     counter++;
     operacion = operacion + widthImg;
     slider.style.transform = `translate(-${operacion}%)`;
     slider.style.transition = "all ease .6s"
-
-}  
+};
 
 const moveToLeft = () => {
     counter--;
-    if (counter < 0 ) {
-        counter = sliderSection.length-1;
-        operacion = widthImg * (sliderSection.length-1)
+    if (counter < 0) {
+        counter = sliderSection.length - 1;
+        operacion = widthImg * (sliderSection.length - 1)
         slider.style.transform = `translate(-${operacion}%)`;
         slider.style.transition = "none";
         return;
-    } 
+    }
     operacion = operacion - widthImg;
     slider.style.transform = `translate(-${operacion}%)`;
     slider.style.transition = "all ease .6s"
+};
 
-}
+btnLeft.addEventListener("click", moveToLeft);
+btnRight.addEventListener("click", moveToRight);
+
+setInterval(() => {
+    moveToRight()
+}, 5000);
+
+let operacion = 0
+let counter = 0
+let widthImg = 100 / sliderSection.length
